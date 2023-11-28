13212564157b8f8c7f61c81421b4b15a8da70ccd
mkmor <Melissa.Morrison@dfo-mpo.gc.ca>
2023-11-28 14:24:51 -0800

    add global variables to suppress R CMD check notes

---
+++ R/global_variables.R
@@ -0,0 +1,9 @@
+# remove NOTE about no visible binding for global variable during R CMD check --
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(
+    c(".", "GOTeDNA_ID", "concentration", "controlType", "detected", "ecodistrict", "eventID",
+      "id", "kingdom", "month", "newP_agg", "newP_yr","scientificName", "target_gene",
+      "target_subfragment", "year"
+    )
+  )
+}
