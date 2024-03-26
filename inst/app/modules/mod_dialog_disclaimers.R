58de91116e06b162b7ce17b0855dfb5e0c0bb694
Kevin Cazelles <kevin.cazelles@insileco.io>
2024-03-26 11:07:01 -0400

    CST-333 integration disclaimer

--- inst/app/modules/mod_dialog_disclaimers.R
+++ inst/app/modules/mod_dialog_disclaimers.R
@@ -2,26 +2,40 @@ mod_dialog_disclaimers_server <- function(id, r) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     query_modal <- modalDialog(
-      title = "Welcome to GOTeDNA!",
+      title = NULL,
+      footer = NULL,
       tagList(
-        p(em("Guidance on Optimal Timing for eDNA"), "is an interactive tool displaying coastal environmental DNA (eDNA) observations."),
-        p("GOTeDNA intends to guide the development of eDNA research and/or monitoring programs based on the observed and predicted optimal detection period(s) for the taxonomic group(s) of interest."),
-        h5("Disclaimers:"),
-        p("This tool intends to infer optimal eDNA detection periods only, not species spatial distribution."),
-        p("Website managers are not responsible for possible errors in the data (e.g., taxonomic misidentification, non-specific amplification, false negative results, etc.). Contact information for data owners is provided for any questions pertaining to the data. "),
-        p("GOTeDNA aims to provide guidance on data trends, gaps and facilitate interpretation to help communication, but we recommend interpreting results with eDNA experts. "),
-        p("This tool does not aim to quantify species abundance from these data, and we strongly caution against doing so."),
-        p("All statistical analyses are provided in a R package at <placeholder>"),
-        br(),
-        p("This tool should be cited as the reference publication: Lacoursière-Roussel, A., Morrison, M., […] (In preparation) Guidance on Optimal Timing for eDNA […]."),
-        p("Data used should cite", em("Data owners"), "Reference is provided.")
+        fluidRow(
+          column(
+            6,
+            div(
+              id = "disclaimer_left",
+              img(
+                src = "img/logo/logo_disclaimer.svg",
+                alt = "Logo GOTeDNA",
+                id = "logo_disclaimer"
+              ),
+              includeHTML(file.path("www", "doc", "welcome.html"))
+            )
+          ),
+          column(
+            6,
+            div(
+              id = "disclaimer_right",
+              includeHTML(file.path("www", "doc", "disclaimer.html")),
+              fluidRow(
+                column(
+                  10,
+                  checkboxInput(ns("agreed"), "I have read and understood the above disclaimers.", value = FALSE, width = "90%")
+                ),
+                column(2, actionButton(ns("dismiss"), "OK", class = "btn-blue"))
+              )
+            )
+          )
+        )
       ),
       easyClose = FALSE,
-      size = "xl",
-      footer = tagList(
-        checkboxInput(ns("agreed"), "I have read and understood the above disclaimers.", value = FALSE, width = "90%"),
-        actionButton(ns("dismiss"), "OK")
-      )
+      size = "xl"
     )
 
     # Show the model on start up ...
