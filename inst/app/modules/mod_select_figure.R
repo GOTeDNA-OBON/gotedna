023f660842069c10387999ca2d583d065bc1cc21
Kevin Cazelles <kevin.cazelles@insileco.io>
2024-03-27 15:04:58 -0400

    CST-307 review figure selection

--- inst/app/modules/mod_select_figure.R
+++ inst/app/modules/mod_select_figure.R
@@ -12,9 +12,9 @@ mod_select_figure_ui <- function(id) {
             4,
             div(
               class = "top_right_button",
-            actionButton(ns("hide_figs"), "Hide/Show figures",
-              title = "Hide or show fields"
-            )
+              actionButton(ns("hide_figs"), "Hide/Show figures",
+                title = "Hide or show fields"
+              )
             )
           )
         )
@@ -24,43 +24,52 @@ mod_select_figure_ui <- function(id) {
         fluidRow(
           column(
             12,
-            actionButton(
-              ns("select_all"),
-              "Select all figures",
-              title = "Select all figures"
+            div(
+              id = "select_all_figures",
+              actionButton(
+                ns("select_all"),
+                "Select all figures",
+                title = "Select all figures"
+              )
             )
           )
         ),
-        fluidRow(
-          add_figure_selection(
-            ns("fig_heatmap"),
-            "Species detection heatmap",
-            "img/thumbnails/tn_heatmap.png"
-          ),
-          add_figure_selection(
-            ns("fig_effort"),
-            "Sample size to achieve detection",
-            "img/thumbnails/tn_effort.png"
-          ),
-          add_figure_selection(
-            ns("fig_higher"),
-            "Field sample size for datasets",
-            "img/thumbnails/tn_higher.png"
-          ),
-          add_figure_selection(
-            ns("fig_detect"),
-            "Monthly eDNA detection probability",
-            "img/thumbnails/tn_thresh.png"
+        div(
+          id = "thumbnail_container",
+          fluidRow(
+            add_figure_selection(
+              ns("fig_heatmap"),
+              "Species detection heatmap",
+              "img/thumbnails/tn_heatmap.png"
+            ),
+            add_figure_selection(
+              ns("fig_effort"),
+              "Sample size to achieve detection",
+              "img/thumbnails/tn_effort.png"
+            ),
+            add_figure_selection(
+              ns("fig_higher"),
+              "Field sample size for datasets",
+              "img/thumbnails/tn_higher.png"
+            ),
+            add_figure_selection(
+              ns("fig_detect"),
+              "Monthly eDNA detection probability",
+              "img/thumbnails/tn_thresh.png"
+            )
           )
         ),
         fluidRow(
           column(
             12,
-            actionButton(
-              ns("confirm"),
-              "Confirm",
-              title = "Confirm selection",
-              class = "btn-blue"
+            div(
+              id = "confirm_figures_selection",
+              actionButton(
+                ns("confirm"),
+                "Confirm",
+                title = "Confirm selection",
+                class = "btn-blue"
+              )
             )
           )
         ),
@@ -105,16 +114,11 @@ mod_select_figure_ui <- function(id) {
         column(
           9,
           div(
-            id = "fig_container",
-            h4("Figure Title"),
-            div(
-              id = "fig_caption",
-              uiOutput(ns("current_cap"), height = "15vh")
-            ),
-            div(
-              id = "fig_panel",
-              plotOutput(ns("current_fig"), height = "65vh")
-            )
+            id = "fig_main_container",
+            ui_figure(1, "Species detection heatmap", "heatmap.html", ns),
+            ui_figure(2, "Sample size to achieve detection", "sample_size.html", ns),
+            ui_figure(3, "Field sample size for ", "field_sample.html", ns),
+            ui_figure(4, "Monthly eDNA detection probability", "detection.html", ns)
           ),
           div(
             id = "reference_data_authorship",
@@ -122,7 +126,8 @@ mod_select_figure_ui <- function(id) {
           ),
         ),
         column(3),
-        column(9,
+        column(
+          9,
           div(
             class = "section_footer",
             actionButton(
@@ -144,10 +149,43 @@ mod_select_figure_server <- function(id, r) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-     observeEvent(input$hide_figs, {
-       shinyjs::toggle("figure_selection_main")
-     })
+    observeEvent(input$hide_figs, {
+      shinyjs::toggle("figure_selection_main")
+    })
 
+    observeEvent(input$select_all, {
+      for (i in c("fig_heatmap", "fig_effort", "fig_higher", "fig_detect")) {
+        shinyjs::show(paste0(i, "_thumbnail_selected"))
+        r$fig_slc[[i]] <- TRUE
+      }
+    })
+
+    shinyjs::hide(paste0("fig_heatmap", "_thumbnail_selected"))
+    observeEvent(input$fig_heatmap, {
+      shinyjs::toggle(paste0("fig_heatmap", "_thumbnail_selected"))
+      r$fig_slc$fig_heatmap <- !r$fig_slc$fig_heatmap
+    })
+
+    shinyjs::hide(paste0("fig_effort", "_thumbnail_selected"))
+    observeEvent(input$fig_effort, {
+      shinyjs::toggle(paste0("fig_effort", "_thumbnail_selected"))
+      r$fig_slc$fig_effort <- !r$fig_slc$fig_effort
+    })
+
+    shinyjs::hide(paste0("fig_higher", "_thumbnail_selected"))
+    observeEvent(input$fig_higher, {
+      shinyjs::toggle(paste0("fig_higher", "_thumbnail_selected"))
+      r$fig_slc$fig_higher <- !r$fig_slc$fig_higher
+    })
+
+    shinyjs::hide(paste0("fig_detect", "_thumbnail_selected"))
+    observeEvent(input$fig_detect, {
+      shinyjs::toggle(paste0("fig_detect", "_thumbnail_selected"))
+      r$fig_slc$fig_detect <- !r$fig_slc$fig_detect
+    })
+
+    # toggle_selected("fig_heatmap", input$fig_heatmap, "fig_heatmap")
+    # shinyjs::hide("fig_heatmap_thumbnail_selected")
 
     observeEvent(input$calc_window, {
       if (r$taxon_slc[4] == "All" && r$data_type == "qPCR") {
@@ -257,6 +295,24 @@ mod_select_figure_server <- function(id, r) {
 }
 
 
+
+ui_figure <- function(fig_id, title, caption_file, ns) {
+  div(
+    id = ns(paste0("fig_container_", fig_id)),
+    class = "fig_container",
+    h4(title),
+    div(
+      class = "fig_caption",
+      includeHTML(file.path("www", "doc", "caption", caption_file))
+    ),
+    div(
+      class = "fig_panel",
+      plotOutput(ns(paste0("fig_plot_area", fig_id)), height = "65vh")
+    )
+  )
+}
+
+
 prepare_data <- function(r) {
   if (length(r$station_slc)) {
     out <- r$data_filtered |>
@@ -335,3 +391,70 @@ draw_fig4 <- function(r, threshold) {
     plotNotAvailable()
   }
 }
+
+plotText <- function(txt, ...) {
+  plot(c(-1, 1), c(-1, 1),
+    ann = FALSE, bty = "n", type = "n", xaxt = "n",
+    yaxt = "n"
+  )
+  text(0, 0, txt, ...)
+}
+
+plotNotAvailableSpeciesLevel <- function() {
+  plotText("Plot only available at the species level.")
+}
+
+plotNotAvailable <- function() {
+  plotText("Plot not available yet.", cex = 1.5)
+}
+
+plotNotAvailableForqPCR <- function() {
+  plotText("Plot not available for qPCR data.")
+}
+
+
+add_thumbnail_button <- function(id, src, title, alt = "Figure thumbnail") {
+  # https://stackoverflow.com/questions/44841346/adding-an-image-to-shiny-action-button
+  tagList(
+    div(
+      class = "thumnail_button_container",
+      tags$button(
+        id = id,
+        class = "btn action-button",
+        tags$img(
+          src = src,
+          alt = alt,
+          id = "fig-thumbnail"
+        ),
+        title = title
+      ),
+      div(
+        id = paste0(id, "_thumbnail_selected"),
+        class = "thumbnail_selected",
+        p("selected")
+      )
+    ),
+  )
+}
+
+add_figure_selection <- function(id, title, scr = NULL, info = title) {
+  column(
+    2,
+    div(
+      class = "thumbnail",
+      add_thumbnail_button(id, scr, info),
+      h5(title)
+    )
+  )
+}
+
+# toggle_selected <- function(class_id, input_button, fig_id) {
+#   cls_id <- paste0(class_id, "_thumbnail_selected")
+#  # browser()
+#   shinyjs::hide(cls_id)
+#   observeEvent(input_button, {
+#     browser()
+#     shinyjs::toggle(cls_id)
+#     r$fig_slc[[fig_id]] <- !r$fig_slc[[fig_id]]
+#   })
+# }
--- inst/app/server.R
+++ inst/app/server.R
@@ -9,6 +9,12 @@ server <- function(input, output, session) {
     show_map_info = FALSE,
     reload_map = 0,
     fig_ready = FALSE,
+    fig_slc = list(
+      fig_heatmap = FALSE, 
+      fig_effort = FALSE,
+      fig_higher = FALSE, 
+      fig_detect = FALSE
+    ),
     current_fig = "fig1",
     lock_view = FALSE
   )
