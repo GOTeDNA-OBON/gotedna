e94390ef187581e93ad09e091a45272a2f6cb33f
Head <Kayley.Head@dfo-mpo.gc.ca>
2026-05-22 08:56:27 -0300

    Adjusted spacing of choice selection page across different screen sizes

--- inst/app/modules/mod_app_choice.R
+++ inst/app/modules/mod_app_choice.R
@@ -9,8 +9,8 @@ mod_app_choice_ui <- function(id) {
       p("Please select which tool you want to launch."),
 
       div(class = "choice-btn-row",
-          actionButton(ns("app_a"), "Explore eDNA Detection Rates", class = "choice-btn"),
-          actionButton(ns("app_b"), "Explore Biodiversity", class = "choice-btn")
+          actionButton(ns("app_a"), "Explore eDNA Study Design", class = "choice-btn"),
+          actionButton(ns("app_b"), "Explore MPA Biodiversity", class = "choice-btn")
       )
     )
   )
